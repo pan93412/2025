e220c1cfccb6405cc9b4732e3e1cd44c7f485042
Yi-Jyun Pan <me@pan93.com>
2025-05-05 20:54:52 +0800

    feat(loaders): add pretalx type-safe fetcher

---
+++ loaders/pretalx/client.ts
@@ -0,0 +1,121 @@
+import type { z } from 'zod'
+import type { MultiLingualString, OptionalMultiLingualString, Room, SessionType, Speaker } from './types'
+import { conference } from '#data/conference.ts'
+import { BadServerSideDataException } from './exception'
+import { coscupSpeakerQuestionIdMap, createResponseSchema, PretalxRoomSchema, PretalxSpeakerSchema, PretalxTalkSchema } from './pretalx-types'
+import { formatMultiLingualString, generateGravatarUrl, getAnswer } from './utils'
+
+export class PretalxApiClient {
+  #endpoint: string
+  #token: string | undefined
+
+  constructor(
+    public readonly year: number,
+    token: string | undefined = undefined,
+  ) {
+    this.#endpoint = `https://pretalx.coscup.org/api/events/coscup-${year}`
+    this.#token = token
+  }
+
+  async getResources<T>(resource: 'talks' | 'rooms' | 'speakers', schema: z.ZodSchema<T>): Promise<T[]> {
+    const responseSchema = createResponseSchema(schema)
+
+    const authHeader = this.#token ? { Authorization: `Token ${this.#token}` } : undefined
+    const response = await fetch(`${this.#endpoint}/${resource}`, {
+      headers: {
+        ...authHeader,
+        'Accept': 'application/json',
+        'User-Agent': `coscup-website-client/${conference.year}`,
+      },
+    })
+    let data = responseSchema.parse(await response.json())
+
+    const results: T[] = []
+
+    do {
+      results.push(...data.results)
+
+      if (data.next) {
+        const response = await fetch(data.next)
+        data = responseSchema.parse(await response.json())
+      }
+    } while (data.next)
+
+    return results
+  }
+
+  async getRooms(): Promise<Room[]> {
+    const pretalxRooms = await this.getResources('rooms', PretalxRoomSchema)
+
+    return pretalxRooms.map((room) => {
+      const name = formatMultiLingualString(room.name)
+      if (!name) {
+        throw new BadServerSideDataException(`Room ${room.id} has empty name.`)
+      }
+
+      return {
+        id: room.id,
+        name,
+      } satisfies Room
+    })
+  }
+
+  async getSpeakers(): Promise<Speaker[]> {
+    const pretalxSpeakers = await this.getResources('speakers', PretalxSpeakerSchema)
+
+    return pretalxSpeakers.map((speaker) => {
+      const chineseName = getAnswer(speaker.answers, coscupSpeakerQuestionIdMap.ZhName)
+      const chineseBio = getAnswer(speaker.answers, coscupSpeakerQuestionIdMap.ZhBio)
+      const englishName = getAnswer(speaker.answers, coscupSpeakerQuestionIdMap.EnName)
+      const englishBio = getAnswer(speaker.answers, coscupSpeakerQuestionIdMap.EnBio)
+      const generalBio = speaker.biography
+
+      if (!speaker.avatar || !speaker.email) {
+        throw new BadServerSideDataException(`Speaker ${speaker.code} has no avatar or email.`)
+      }
+
+      if (!chineseName && !englishName) {
+        throw new BadServerSideDataException(`Speaker ${speaker.code} has no valid Chinese and English name.`)
+      }
+
+      return {
+        id: speaker.code,
+        avatar: speaker.avatar ?? generateGravatarUrl(speaker.email),
+        name: {
+          'zh-tw': (chineseName ?? englishName)!,
+          'en': (englishName ?? chineseName)!,
+        } satisfies MultiLingualString,
+        bio: {
+          'zh-tw': chineseBio ?? englishBio ?? generalBio,
+          'en': englishBio ?? chineseBio ?? generalBio,
+        } satisfies OptionalMultiLingualString,
+      } satisfies Speaker
+    })
+  }
+
+  async getSessionTypes(): Promise<Set<SessionType>> {
+    const sessionType = new Map<string, SessionType>()
+    const pretalxTalks = await this.getResources('talks', PretalxTalkSchema)
+
+    for (const talk of pretalxTalks) {
+      const track = talk.track
+      if (!track) {
+        continue
+      }
+
+      const trackChineseName = track['zh-tw'] ?? track.en
+      const trackEnglishName = track.en ?? track['zh-tw']
+
+      if (!trackChineseName || !trackEnglishName) {
+        throw new BadServerSideDataException(`The talk ${talk.code} ("${talk.title}") has an track name that does not have any valid name.`)
+      }
+
+      sessionType.set(trackChineseName, {
+        'zh-tw': trackChineseName,
+        'en': trackEnglishName,
+      } satisfies SessionType)
+    }
+
+    return new Set(sessionType.values())
+  }
+}
---
+++ loaders/pretalx/pretalx-types.ts
@@ -0,0 +1,130 @@
+import { z } from 'zod'
+
+export const PretalxMultiLingualStringSchema = z.object({
+  'en': z.string().optional(),
+  'zh-tw': z.string().optional(),
+})
+
+export type PretalxMultiLingualString = z.infer<typeof PretalxMultiLingualStringSchema>
+
+export const PretalxQuestionSchema = z.object({
+  id: z.number(),
+  question: PretalxMultiLingualStringSchema,
+  required: z.boolean(),
+  target: z.string(),
+  options: z.unknown().array(),
+})
+
+export type PretalxQuestion = z.infer<typeof PretalxQuestionSchema>
+
+export const PretalxAnswerSchema = z.object({
+  id: z.number(),
+  question: PretalxQuestionSchema,
+  answer: z.string(),
+  answer_file: z.string().nullable(),
+  submission: z.string(),
+  person: z.unknown().nullable(),
+  options: z.array(z.object({
+    answer: z.object({
+      en: z.string(),
+    }),
+  })),
+})
+
+export type PretalxAnswer = z.infer<typeof PretalxAnswerSchema>
+
+export const PretalxSlotSchema = z.object({
+  start: z.string(),
+  end: z.string(),
+  room: z.union([
+    z.object({
+      room_id: z.number().optional(),
+    }),
+    PretalxMultiLingualStringSchema,
+  ]),
+})
+
+export type PretalxSlot = z.infer<typeof PretalxSlotSchema>
+
+export const PretalxSpeakerReferenceSchema = z.object({
+  name: z.string(),
+  code: z.string(),
+  biography: z.string(),
+  email: z.string().optional(),
+})
+
+export type PretalxSpeakerReference = z.infer<typeof PretalxSpeakerReferenceSchema>
+
+export const PretalxTalkSchema = z.object({
+  code: z.string(),
+  speakers: PretalxSpeakerReferenceSchema.array(),
+  title: z.string(),
+  submission_type: z.object({
+    en: z.string(),
+  }),
+  track: PretalxMultiLingualStringSchema.nullable(),
+  track_id: z.number().optional(),
+  state: z.enum(['submitted', 'accepted', 'rejected', 'confirmed']),
+  abstract: z.string(),
+  description: z.string(),
+  duration: z.number().nullable(),
+  do_not_record: z.boolean(),
+  is_featured: z.boolean(),
+  content_locale: z.string(),
+  slot: PretalxSlotSchema,
+  answers: PretalxAnswerSchema.array(),
+  tags: z.string().array().optional(),
+})
+
+export type PretalxTalk = z.infer<typeof PretalxTalkSchema>
+
+export const PretalxRoomSchema = z.object({
+  id: z.number(),
+  name: PretalxMultiLingualStringSchema,
+  description: PretalxMultiLingualStringSchema,
+  capacity: z.number(),
+  position: z.number(),
+})
+
+export type PretalxRoom = z.infer<typeof PretalxRoomSchema>
+
+export const PretalxSpeakerSchema = z.object({
+  code: z.string(),
+  name: z.string(),
+  biography: z.string(),
+  submissions: z.string().array(),
+  avatar: z.string(),
+  email: z.string().optional(),
+  answers: PretalxAnswerSchema.array(),
+})
+
+export type PretalxSpeaker = z.infer<typeof PretalxSpeakerSchema>
+
+export function createResponseSchema<T>(schema: z.ZodSchema<T>) {
+  return z.object({
+    count: z.number(),
+    next: z.string().nullable(),
+    previous: z.string().nullable(),
+    results: schema.array(),
+  })
+}
+
+export type PretalxResponse<T> = z.infer<ReturnType<typeof createResponseSchema<T>>>
+
+export const coscupSessionQuestionIdMap = {
+  Language: 216,
+  ZhDesc: 0,
+  EnTitle: 173,
+  EnDesc: 175,
+  Tags: 220,
+  Qa: 0,
+  Slide: 0,
+  Record: 2098,
+} as const
+
+export const coscupSpeakerQuestionIdMap = {
+  ZhName: 0,
+  ZhBio: 0,
+  EnName: 0,
+  EnBio: 174,
+} as const
