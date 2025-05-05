9389d2f809c8267b9ffdccc89643195c5129fa2e
Yi-Jyun Pan <me@pan93.com>
2025-05-05 22:24:57 +0800

    feat(programs): async loading example

---
+++ content/zh_tw/programs/speakers.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { data: speakers } = await import('#loaders/speakers.data')
+</script>
+
+<template>
+  <ul>
+    <li
+      v-for="speaker in speakers"
+      :key="speaker.id"
+    >
+      <img
+        :alt="speaker.name['zh-tw']"
+        :src="speaker.avatar"
+        width="30"
+      >
+      {{ speaker.name['zh-tw'] }} / {{ speaker.name.en }}
+    </li>
+  </ul>
+</template>
