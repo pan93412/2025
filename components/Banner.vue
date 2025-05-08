bf8b1e432e9e71491fd3a0c707cad716a9c1a926
Mirumo <mysteryperson520@gmail.com>
2025-05-08 19:41:07 +0800

    Feat: add banner before hero page

---
+++ components/Banner.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import mobileBanner from '#assets/images/banner-mobile.png'
+import banner from '#assets/images/banner.png'
+</script>
+
+<template>
+  <div class="banner">
+    <img
+      alt="COSCUP x RubyConf Taiwan 2025 banner"
+      :src="banner"
+    >
+    <img
+      alt="COSCUP x RubyConf Taiwan 2025 mobile banner"
+      class="mobile"
+      :src="mobileBanner"
+    >
+  </div>
+</template>
+
+<style scoped>
+@media screen and (min-width: 513px) {
+  .banner > .mobile {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 512px) {
+  .banner > :not(.mobile) {
+    display: none;
+  }
+}
+</style>
