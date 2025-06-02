30bffdd265d97db33f8f482940c02eec6c738495
Mirumo <mysteryperson520@gmail.com>
2025-06-02 22:41:19 +0800

    refactor: switch from two-column to single-column

--- content/zh_tw/sponsor/index.vue
+++ content/zh_tw/sponsor/index.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import SponsorOneColumn from './SponsorOneColumn.vue'
-import SponsorTwoColumns from './SponsorTwoColumns.vue'
 </script>
 
 <template>
   <SponsorOneColumn
-    :sponsor-levels="['titanium']"
-  />
-  <SponsorTwoColumns
     :sponsor-levels="[
+      'titanium',
       'gold',
       'silver',
       'bronze',
