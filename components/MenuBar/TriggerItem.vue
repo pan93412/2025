689c0bc655243330f87c49dd051cc729d66d9995
Yi-Jyun Pan <me@pan93.com>
2025-06-13 15:12:48 +0800

    feat(components): add MenuBar

---
+++ components/MenuBar/TriggerItem.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+const props = withDefaults(defineProps<{
+  selected?: boolean
+}>(), {
+  selected: false,
+})
+</script>
+
+<template>
+  <button
+    :class="[$style.triggerItem, { [$style.selected]: props.selected }]"
+  >
+    <slot />
+  </button>
+</template>
+
+<style module>
+.triggerItem {
+  width: 100%;
+  border-radius: 4px;
+  padding: 2px 8px;
+  text-align: center;
+  font-size: 14px;
+  color: #364153;
+  font-family: 'PingFang TC';
+  letter-spacing: 0.03em;
+}
+
+.selected {
+  background-color: #e5e3ff;
+  color: #4c4598;
+}
+</style>
