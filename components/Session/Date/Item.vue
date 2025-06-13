5d2cd662b7c4561a0a0833004f1605ca92607c4b
Yi-Jyun Pan <me@pan93.com>
2025-06-13 15:12:36 +0800

    feat(components): add session/date

---
+++ components/Session/Date/Item.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  active?: boolean
+}>(), {
+  active: false,
+})
+</script>
+
+<template>
+  <div :class="[$style.dateTab, { [$style.active]: props.active }]">
+    <slot />
+  </div>
+</template>
+
+<style module>
+.dateTab {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  border-radius: 36em;
+  border: 3px solid #364153;
+  box-shadow: 0 4px 0 #364153;
+  background-color: #d1d5dc;
+  color: #fff;
+  font-size: 1.75em;
+  font-weight: 700;
+  font-style: italic;
+  font-family: Inter;
+  line-height: 1.2;
+  text-align: center;
+}
+
+.active {
+  background-color: #7f73fe;
+}
+</style>
