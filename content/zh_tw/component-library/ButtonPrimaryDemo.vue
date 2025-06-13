93f305cb2d1cd841a5fc5a523229955332e7d0e3
Yi-Jyun Pan <me@pan93.com>
2025-06-13 15:42:43 +0800

    feat: add component-library page

---
+++ content/zh_tw/component-library/ButtonPrimaryDemo.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Button from '#components/Button.vue'
+</script>
+
+<template>
+  <div :class="$style.container">
+    <Button variant="primary">
+      Plain Primary Button
+    </Button>
+
+    <Button variant="primary">
+      <template #icon>
+        <IconPhCalendarPlus />
+      </template>
+
+      Primary Button with Icon
+    </Button>
+
+    <Button
+      disabled
+      variant="primary"
+    >
+      Disabled Primary Button
+    </Button>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-top: 12px;
+}
+</style>
---
+++ content/zh_tw/component-library/MenuBarDemo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MenuBarGroup from '#components/MenuBar/Group.vue'
+import MenuBarTriggerItem from '#components/MenuBar/TriggerItem.vue'
+</script>
+
+<template>
+  <MenuBarGroup>
+    <MenuBarTriggerItem>議程</MenuBarTriggerItem>
+    <MenuBarTriggerItem :selected="true">
+      我的收藏
+    </MenuBarTriggerItem>
+  </MenuBarGroup>
+</template>
