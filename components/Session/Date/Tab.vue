05be3209226e1db7ac7a026194ee605857c35cb3
Yi-Jyun Pan <me@pan93.com>
2025-06-13 15:12:36 +0800

    feat(components): add session/date

---
+++ components/Session/Date/Tab.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="$style.tab">
+    <slot />
+  </div>
+</template>
+
+<style module>
+.tab {
+  display: flex;
+  gap: 7px;
+  font-family: Inter;
+  color: #fff;
+}
+
+.tab > * {
+  flex: 1;
+}
+</style>
