84db28efcf18a7d5bd2c61031b6794df4c218d46
Riley Ho <riley@rileychh.dev>
2025-06-29 14:41:50 +0800

    feat(component-library): add IconButtonBasicDemo and IconButtonPrimaryDemo components

---
+++ content/zh_tw/component-library/IconButtonBasicDemo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import IconButton from '#components/IconButton.vue'
+</script>
+
+<template>
+  <div :class="$style.container">
+    <IconButton variant="basic">
+      <IconPhCalendarPlus />
+    </IconButton>
+
+    <IconButton variant="basic">
+      <IconPhHeart />
+    </IconButton>
+
+    <IconButton
+      disabled
+      variant="basic"
+    >
+      <IconPhCalendarPlus />
+    </IconButton>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-top: 12px;
+}
+</style>
