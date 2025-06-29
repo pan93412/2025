dc8d4be7efaefed25bdeecca1fb1de4604715d3e
Riley Ho <riley@rileychh.dev>
2025-06-29 15:39:00 +0800

    feat(component-library): add CheckboxDemo component and update index

---
+++ content/zh_tw/component-library/CheckboxDemo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Checkbox from '#components/Checkbox.vue'
+</script>
+
+<template>
+  <div :class="$style.container">
+    <Checkbox name="checkbox1">
+      Unchecked
+    </Checkbox>
+
+    <Checkbox
+      checked
+      name="checkbox2"
+    >
+      Checked
+    </Checkbox>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-top: 12px;
+}
+</style>
