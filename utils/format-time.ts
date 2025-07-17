4934a5cd6d63c27e1ffe04ac7cb447dc7f6495e5
Yi-Jyun Pan <me@pan93.com>
2025-07-17 19:41:20 +0800

    fix(format-time): lock timezone to Asia/Taipei

--- utils/format-time.ts
+++ utils/format-time.ts
@@ -7,17 +7,20 @@ export function formatTimeRange(startAt: string | Date, endAt: string | Date, wi
         year: 'numeric',
         month: 'long',
         day: '2-digit',
+        timeZone: 'Asia/Taipei',
       })
     : ''
   const startTimeString = startDate.toLocaleTimeString('zh-TW', {
     hour: '2-digit',
     minute: '2-digit',
     hour12: false,
+    timeZone: 'Asia/Taipei',
   })
   const endTimeString = endDate.toLocaleTimeString('zh-TW', {
     hour: '2-digit',
     minute: '2-digit',
     hour12: false,
+    timeZone: 'Asia/Taipei',
   })
 
   return `${dateString} ${startTimeString} ~ ${endTimeString}`.trim()
